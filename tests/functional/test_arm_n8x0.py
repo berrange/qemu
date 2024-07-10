44e869bdbe77e5ad4c797e3011f4c80e958fea12
Thomas Huth <thuth@redhat.com>
2024-07-10 13:00:43 +0200

    tests/functional: Convert some tests that download files via fetch_asset()
    Now that we've got the Asset class with pre-caching, we can convert
    some Avocado tests that use fetch_asset() for downloading their
    required files.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- tests/functional/test_arm_n8x0.py
+++ tests/functional/test_arm_n8x0.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+#
 # Functional test that boots a Linux kernel and checks the console
 #
 # Copyright (c) 2020 Red Hat, Inc.
@@ -10,21 +12,22 @@
 
 import os
 
-from avocado import skipUnless
-from avocado_qemu import QemuSystemTest
-from avocado_qemu import wait_for_console_pattern
+from unittest import skipUnless
+from qemu_test import QemuSystemTest, Asset
+from qemu_test import wait_for_console_pattern
 
 class N8x0Machine(QemuSystemTest):
     """Boots the Linux kernel and checks that the console is operational"""
 
     timeout = 90
 
+    ASSET_KERNEL = Asset(('http://stskeeps.subnetmask.net/meego-n8x0/'
+                          'meego-arm-n8x0-1.0.80.20100712.1431-'
+                          'vmlinuz-2.6.35~rc4-129.1-n8x0'),
+                         'e9d5ab8d7548923a0061b6fbf601465e479ed269')
+
     def __do_test_n8x0(self):
-        kernel_url = ('http://stskeeps.subnetmask.net/meego-n8x0/'
-                      'meego-arm-n8x0-1.0.80.20100712.1431-'
-                      'vmlinuz-2.6.35~rc4-129.1-n8x0')
-        kernel_hash = 'e9d5ab8d7548923a0061b6fbf601465e479ed269'
-        kernel_path = self.fetch_asset(kernel_url, asset_hash=kernel_hash)
+        kernel_path = self.ASSET_KERNEL.fetch()
 
         self.vm.set_console(console_index=1)
         self.vm.add_args('-kernel', kernel_path,
@@ -32,18 +35,15 @@ class N8x0Machine(QemuSystemTest):
         self.vm.launch()
         wait_for_console_pattern(self, 'TSC2005 driver initializing')
 
-    @skipUnless(os.getenv('AVOCADO_ALLOW_UNTRUSTED_CODE'), 'untrusted code')
+    @skipUnless(os.getenv('QEMU_TEST_ALLOW_UNTRUSTED_CODE'), 'untrusted code')
     def test_n800(self):
-        """
-        :avocado: tags=arch:arm
-        :avocado: tags=machine:n800
-        """
+        self.set_machine('n800')
         self.__do_test_n8x0()
 
-    @skipUnless(os.getenv('AVOCADO_ALLOW_UNTRUSTED_CODE'), 'untrusted code')
+    @skipUnless(os.getenv('QEMU_TEST_ALLOW_UNTRUSTED_CODE'), 'untrusted code')
     def test_n810(self):
-        """
-        :avocado: tags=arch:arm
-        :avocado: tags=machine:n810
-        """
+        self.set_machine('n810')
         self.__do_test_n8x0()
+
+if __name__ == '__main__':
+    QemuSystemTest.main()
